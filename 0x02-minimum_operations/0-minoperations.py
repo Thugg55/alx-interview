e94eed2a37d5499336978e806d085856b1a77962
Thugg55 <thuggeru7@gmail.com>
2024-07-11 20:50:39 -0700

    task done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python module for minimum operations
+"""
+
+import math
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
